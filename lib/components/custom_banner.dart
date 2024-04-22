77748f8ad30b3f81a487a6217a6ba2d1d52c1d4b
박현우 <simuoo96@gmail.com>
2024-04-22 15:08:51 +0900

    feat : banner 구현

---
+++ lib/components/custom_banner.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:project/dto/banenr_controller.dart';
+
+class CustomBanner extends StatelessWidget {
+  final BannerController bannerController = Get.put(BannerController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(top: 2),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Stack(
+                children: [
+                  PageView(
+                    controller: bannerController.pageController,
+                    onPageChanged: (int page) {
+                      bannerController.changePage(page);
+                    },
+                    children: [
+                      buildBanner('광고 1', 0),
+                      buildBanner('광고 2', 1),
+                    ],
+                  ),
+                  Obx(() => Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      margin: EdgeInsets.symmetric(vertical: 5.0), // 높이
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: List.generate(2, (i) => Container( // 개수
+                          margin: EdgeInsets.all(2.0), // 간격
+                          width: 8,
+                          height: 8,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: bannerController.currentPage.value == i
+                                ? Color(0xFFffffff)
+                                : Color(0xFFffffff).withOpacity(0.5),
+                          ),
+                        )),
+                      ),
+                    ),
+                  )),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildBanner(String text, int index) {
+    Color appBarColor = Color.fromRGBO(121,159,165,1.0);
+    return Container(
+      height: 140,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(3.0),
+        color: appBarColor,
+      ),
+      child: Center(child: Text(text)),
+    );
+  }
+}
