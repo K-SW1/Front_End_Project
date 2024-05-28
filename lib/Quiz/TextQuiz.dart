f149615122c65e0c72cb092de4466a5c50ec4b79
박현우 <simuoo96@gmail.com>
2024-05-28 15:08:31 +0900

    feat : TextQuiz 구현

--- lib/Quiz/TextQuiz.dart
+++ lib/Quiz/TextQuiz.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:project/components/custom_appbar.dart';
 import 'package:project/components/custom_banner.dart';
+import 'package:project/components/custom_textquiz_answer.dart';
 import 'package:project/components/custom_textquiz_form.dart';
 
 class TextQuiz extends StatelessWidget {
@@ -14,7 +15,7 @@ class TextQuiz extends StatelessWidget {
           child: Center(
             child: Column(
               children: [
-                SizedBox(height: 10),
+                SizedBox(height: 20),
                 Text(
                   "우리 가족 퀴즈 문제 등록",
                   textAlign: TextAlign.center,
@@ -23,7 +24,7 @@ class TextQuiz extends StatelessWidget {
                     fontSize: 25,
                   ),
                 ),
-                SizedBox(height: 100),
+                SizedBox(height: 70),
                 Text(
                   "문제 내용",
                   textAlign: TextAlign.center,
@@ -33,7 +34,7 @@ class TextQuiz extends StatelessWidget {
                   ),
                 ),
                 CustomTextForm(),
-                SizedBox(height: 46),
+                SizedBox(height: 53),
                 Text(
                   "힌트 내용",
                   textAlign: TextAlign.center,
@@ -43,8 +44,70 @@ class TextQuiz extends StatelessWidget {
                   ),
                 ),
                 CustomTextForm(),
-                Text("\n\n\n\n\n\n\n\n\n"),
-
+                SizedBox(height: 47),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          "보기 1",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        CustomTextAnswer(),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "보기 2",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        CustomTextAnswer(),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "보기 3",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        CustomTextAnswer(),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "정답",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Color.fromRGBO(121, 159, 165, 1.0),
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        CustomTextAnswer(),
+                      ],
+                    ),
+                  ],
+                ),
+                Text("\n"),
                 SizedBox(height: 30),
                 Container(
                   height: 140,
