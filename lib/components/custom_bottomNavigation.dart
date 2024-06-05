ce035157bbe551e259ab52cb557c1dafabbc8327
박현우 <simuoo96@gmail.com>
2024-06-05 10:34:48 +0900

    feat : Bottom 구현

---
+++ lib/components/custom_bottomNavigation.dart
@@ -0,0 +1,20 @@
+// import 'package:flutter/material.dart';
+//
+// class CustomBottom extends StatelessWidget {
+//   Color appBarColor = Color.fromRGBO(121,159,165,1.0);
+//   @override
+//   Widget build(BuildContext context) {
+//     return BottomNavigationBar(
+//         elevation: 5,
+//         selectedItemColor: appBarColor,
+//         unselectedItemColor: appBarColor,
+//         selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+//         unselectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+//         items: <BottomNavigationBarItem> [
+//           BottomNavigationBarItem(icon: Icon(Icons.phone, size: 45,)),
+//           BottomNavigationBarItem(icon: Icon(Icons.home,size: 45,)),
+//           BottomNavigationBarItem(icon: Icon(Icons.account_circle,size: 45,)),
+//         ]
+//     );
+//   }
+// }
