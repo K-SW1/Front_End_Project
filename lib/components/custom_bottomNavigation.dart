0114a45fb145dcd799646583d7483cf1cf7bc8e6
박현우 <simuoo96@gmail.com>
2024-07-05 13:59:02 +0900

    feat : bottomNavigation 구현

--- lib/components/custom_bottomNavigation.dart
+++ lib/components/custom_bottomNavigation.dart
@@ -1,20 +1,57 @@
-// import 'package:flutter/material.dart';
-//
-// class CustomBottom extends StatelessWidget {
-//   Color appBarColor = Color.fromRGBO(121,159,165,1.0);
-//   @override
-//   Widget build(BuildContext context) {
-//     return BottomNavigationBar(
-//         elevation: 5,
-//         selectedItemColor: appBarColor,
-//         unselectedItemColor: appBarColor,
-//         selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
-//         unselectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
-//         items: <BottomNavigationBarItem> [
-//           BottomNavigationBarItem(icon: Icon(Icons.phone, size: 45,)),
-//           BottomNavigationBarItem(icon: Icon(Icons.home,size: 45,)),
-//           BottomNavigationBarItem(icon: Icon(Icons.account_circle,size: 45,)),
-//         ]
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:project/Calender/calender.dart';
+import 'package:project/Quiz/TextQuiz.dart';
+import 'package:project/Main/MainHome.dart';
+
+class CustomBottom extends StatefulWidget {
+  @override
+  State<CustomBottom> createState() => CustomBottomState();
+}
+
+class CustomBottomState extends State<CustomBottom> {
+  Color appBarColor = Color.fromRGBO(121, 159, 165, 1.0);
+  int _selectedIndex = 0;
+
+  final List<Widget> _navIndex = [
+    MainHome(),
+    CalenderApp(),
+    TextQuiz(),
+  ];
+
+  void _onNavTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      switch (index) {
+        case 0:
+          Get.to(() => CalenderApp());
+          break;
+        case 1:
+          Get.to(() => Home());
+          break;
+        case 2:
+          Get.to(() => TextQuiz());
+          break;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: _selectedIndex,
+      onTap: _onNavTapped,
+      selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+      unselectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+      selectedItemColor: appBarColor,
+      unselectedItemColor: appBarColor,
+      showUnselectedLabels: true,
+      type: BottomNavigationBarType.fixed,
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.phone,size: 45), label: ""),
+        BottomNavigationBarItem(icon: Icon(Icons.home,size: 45), label: ""),
+        BottomNavigationBarItem(icon: Icon(Icons.account_circle,size: 45), label: ""),
+      ],
+    );
+  }
+}
