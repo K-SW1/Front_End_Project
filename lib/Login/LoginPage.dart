1ad893188459f904ebb8ab858b004f785ffdbd2b
박현우 <simuoo96@gmail.com>
2024-07-15 21:08:33 +0900

    feat : 로그인-DB 연동

--- lib/Login/LoginPage.dart
+++ lib/Login/LoginPage.dart
@@ -1,33 +1,122 @@
+// import 'package:flutter/material.dart';
+// import 'package:project/Custom/custom_size.dart';
+//
+// class LoginPage extends StatelessWidget {
+//   LoginPage({Key? key}):super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Color.fromRGBO(121,159,165,1.0),
+//       body: Padding(
+//         padding: EdgeInsets.all(16.0),
+//         child: ListView(
+//           children: [
+//             SizedBox(height: 100),
+//             Center(
+//               child: Image(
+//                 image: AssetImage('assets/images/recall_splash.png'),
+//                 fit: BoxFit.contain,
+//                 color: Colors.black,
+//               ),
+//             ),
+//             SizedBox(height: xlarge_gap,),
+//             SizedBox(height: large_gap,),
+//             // CustomForm(),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:project/Custom/custom_size.dart';
-import 'package:project/components/custom_form.dart';
+import 'package:http/http.dart' as http;
+import 'package:project/Main/MainHome.dart';
 
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
 
-class LoginPage extends StatelessWidget {
-  LoginPage({Key? key}):super(key: key);
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  Future<void> login() async {
+    String url = 'http://localhost:8080/auth/login';
+
+    Map<String, String> headers = {
+      'Content-Type': 'application/json',
+    };
+
+    Map<String, dynamic> body = {
+      'username': usernameController.text,
+      'password': passwordController.text,
+    };
+
+    try {
+      var response = await http.post(
+        Uri.parse(url),
+        headers: headers,
+        body: jsonEncode(body),
+      );
+
+      if (response.statusCode == 200) {
+        // Successful login
+        print('Login successful');
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => MainHome()),
+        );  // Navigate to MainHome
+      } else {
+        // Handle other status codes, such as 4xx or 5xx errors
+        print('Failed to login. Status code: ${response.statusCode}');
+
+      }
+    } catch (e) {
+      // Handle network or server errors
+      print('Exception occurred while logging in: $e');
+
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(121,159,165,1.0),
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: ListView(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(height: 100),
-            Center(
-              child: Image(
-                image: AssetImage('assets/images/recall_splash.png'),
-                fit: BoxFit.contain,
-                color: Colors.black,
-              ),
+            TextField(
+              controller: usernameController,
+              decoration: InputDecoration(labelText: 'Username'),
             ),
-            SizedBox(height: xlarge_gap,),
-            SizedBox(height: large_gap,),
-            CustomForm(),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: login,
+              child: Text('Login'),
+            ),
           ],
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: LoginPage(),
+  ));
 }
