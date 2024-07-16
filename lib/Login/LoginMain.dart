050f0d8f0937658eee45f92abebb311bfafadfdc
박현우 <simuoo96@gmail.com>
2024-07-16 14:57:20 +0900

    docs : 오류 수정

--- lib/Login/LoginMain.dart
+++ lib/Login/LoginMain.dart
@@ -1,32 +1,32 @@
-import 'package:flutter/material.dart';
-
-import 'package:project/Login/LoginPage.dart';
-
-void main() => runApp(LoginMain());
-
-class LoginMain extends StatelessWidget {
-  const LoginMain({Key? key}):super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(
-            backgroundColor: Colors.black,
-            primary: Color.fromRGBO(121,159,165,1.0),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30),
-            ),
-            minimumSize: Size(400,60),
-          )
-        )
-      ),
-      initialRoute: "/login",
-      routes: {
-        "/login":(context) => LoginPage(),
-      },
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+//
+// import 'package:project/Login/LoginPage.dart';
+//
+// void main() => runApp(LoginMain());
+//
+// class LoginMain extends StatelessWidget {
+//   const LoginMain({Key? key}):super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         textButtonTheme: TextButtonThemeData(
+//           style: TextButton.styleFrom(
+//             backgroundColor: Colors.black,
+//             primary: Color.fromRGBO(121,159,165,1.0),
+//             shape: RoundedRectangleBorder(
+//               borderRadius: BorderRadius.circular(30),
+//             ),
+//             minimumSize: Size(400,60),
+//           )
+//         )
+//       ),
+//       initialRoute: "/login",
+//       routes: {
+//         "/login":(context) => LoginPage(),
+//       },
+//     );
+//   }
+// }
