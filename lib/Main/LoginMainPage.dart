5503a85b235141eb303da01323c194bf29cfcb03
박현우 <simuoo96@gmail.com>
2024-10-22 03:09:38 +0900

    docs : 로그인 메인페이지 수정

--- lib/Main/LoginMainPage.dart
+++ lib/Main/LoginMainPage.dart
@@ -1,116 +1,7 @@
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:project/Dementia/DementiaTest.dart';
-// import 'package:project/Login/LoginMain.dart';
-// import 'package:project/Regist/RegistAgree.dart';
-//
-//
-// void main() => runApp(LoginPage());
-//
-// class LoginPage extends StatelessWidget {
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     Color appBarColor = Color.fromRGBO(121,159,165,1.0);
-//     return GetMaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: Scaffold(
-//         backgroundColor: appBarColor,
-//         body: Center(
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             crossAxisAlignment: CrossAxisAlignment.center,
-//             children: [
-//               InkWell(
-//                 onTap: () {
-//                   Get.to(() => LoginMain());
-//                 },
-//                 child: Container(
-//                   margin: EdgeInsets.only(bottom: 70),
-//                   width: 180,
-//                   height: 100,
-//                   child: Center(
-//                     child: Text(
-//                       "로그인",
-//                       style: TextStyle(
-//                         fontWeight: FontWeight.bold,
-//                         fontSize: 30,
-//                       ),
-//                     ),
-//                   ),
-//                   decoration: BoxDecoration(
-//                     borderRadius: BorderRadius.circular(35),
-//                     border: Border.all(color: Colors.black,width: 3),
-//                     color: Colors.white,
-//                   ),
-//                 ),
-//               ),
-//
-//               InkWell(
-//                 onTap: () {
-//                   Get.to(() => TermsOfServiceAgreement());
-//                 },
-//                 child: Container(
-//                   padding: EdgeInsets.symmetric(vertical: 10),
-//                   width: 180,
-//                   height: 100,
-//                   child: Center(
-//                     child: Text(
-//                       "회원가입",
-//                       style: TextStyle(
-//                         fontWeight: FontWeight.bold,
-//                         fontSize: 30,
-//                       ),
-//                     ),
-//                   ),
-//                   decoration: BoxDecoration(
-//                     borderRadius: BorderRadius.circular(35),
-//                     border: Border.all(color: Colors.black, width: 3),
-//                     color: Colors.white
-//                   ),
-//                 ),
-//               ),
-//
-//               SizedBox(height: 60,),
-//               InkWell(
-//                 onTap: () {
-//                   Get.to(() => DementiaTest());
-//                 },
-//                 child: Container(
-//                   padding: EdgeInsets.symmetric(vertical: 10),
-//                   width: 180,
-//                   height: 100,
-//                   child: Center(
-//                     child: Text(
-//                       "치매테스트",
-//                       style: TextStyle(
-//                         fontWeight: FontWeight.bold,
-//                         fontSize: 30,
-//                       ),
-//                     ),
-//                   ),
-//                   decoration: BoxDecoration(
-//                       borderRadius: BorderRadius.circular(35),
-//                       border: Border.all(color: Colors.black, width: 3),
-//                       color: Colors.white
-//                   ),
-//                 ),
-//               ),
-//
-//               SizedBox(height: 90),
-//               Image.asset("assets/images/recall_splash.png",height: 40,),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:project/Dementia/DementiaTest.dart';
+import 'package:project/Login/LoginMain.dart';
 import 'package:project/Login/LoginPage.dart';
 import 'package:project/Regist/RegistAgree.dart';
 
@@ -130,6 +21,8 @@ class LoginPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            SizedBox(height: 100),
+
             InkWell(
               onTap: () {
                 Get.off(() => Login());
@@ -154,9 +47,10 @@ class LoginPage extends StatelessWidget {
                 ),
               ),
             ),
+
             InkWell(
               onTap: () {
-                Get.to(() => TermsOfServiceAgreement());
+                Get.off(() => IdRegist());
               },
               child: Container(
                 padding: EdgeInsets.symmetric(vertical: 10),
@@ -178,10 +72,12 @@ class LoginPage extends StatelessWidget {
                 ),
               ),
             ),
+
             SizedBox(height: 60),
+
             InkWell(
               onTap: () {
-                Get.to(() => DementiaTest());
+                Get.off(() => DementiaTest());
               },
               child: Container(
                 padding: EdgeInsets.symmetric(vertical: 10),
@@ -203,7 +99,9 @@ class LoginPage extends StatelessWidget {
                 ),
               ),
             ),
+
             SizedBox(height: 90),
+
             Image.asset("assets/images/recall_splash.png", height: 40),
           ],
         ),
