0e71ad2d4648e528a1d34bca5748bc9395ddd6a0
박현우 <simuoo96@gmail.com>
2024-10-22 03:07:18 +0900

    docs : 네이버지도 구현

--- lib/Main/MainHome.dart
+++ lib/Main/MainHome.dart
@@ -1,9 +1,110 @@
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:project/Calender/calender.dart';
+// import 'package:project/Missing/Missing.dart';
+// import 'package:project/Quiz/QuizSelect.dart';
+// import 'package:project/aasd/teste.dart';
+//
+// import 'package:project/components/custom_banner.dart';
+// import 'package:project/components/custom_appbar.dart';
+// import 'package:project/components/custom_bottomNavigation.dart';
+//
+// void main() => runApp(MH());
+//
+// class MH extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: MainHome(),
+//     );
+//   }
+// }
+//
+// class MainHome extends StatelessWidget {
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: CustomAppBar(),  // CustomAppBar이 올바르게 구현되어 있어야 합니다.
+//       body: SingleChildScrollView(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//           children: [
+//             _buildMenuItem(
+//               icon: Icons.calendar_month,
+//               text: "돌봄 다이어리",
+//               onTap: () => Get.to(() => CalenderApp()),
+//             ),
+//             _buildMenuItem(
+//               icon: Icons.grain,
+//               text: "데이케어 센터 검색",
+//               onTap: () => Get.to(() => NMAP()),
+//             ),
+//             _buildMenuItem(
+//               icon: Icons.family_restroom,
+//               text: "우리 가족 퀴즈",
+//               onTap: () => Get.to(() => QuizSelect()),
+//             ),
+//             _buildMenuItem(
+//               icon: Icons.emoji_people,
+//               text: "어르신 길 잃음 방지",
+//               onTap: () => Get.to(() => Missing()),
+//             ),
+//             SizedBox(height: 23),
+//             Container(
+//               height: 140,
+//               child: CustomBanner(),  // CustomBanner가 올바르게 구현되어 있는지 확인하세요.
+//             ),
+//           ],
+//         ),
+//       ),
+//       bottomNavigationBar: CustomBottom(),  // CustomBottom이 올바르게 구현되어 있는지 확인하세요.
+//     );
+//   }
+//
+//   Widget _buildMenuItem({
+//     required IconData icon,
+//     required String text,
+//     required VoidCallback onTap,
+//   }) {
+//     return Column(
+//       children: [
+//         InkWell(
+//           onTap: onTap,
+//           child: Container(
+//             width: 400,
+//             height: 100,
+//             color: Colors.white10,
+//             child: Row(
+//               mainAxisAlignment: MainAxisAlignment.start,
+//               children: [
+//                 Icon(icon, size: 45, color: Colors.black),
+//                 SizedBox(width: 20),
+//                 Text(
+//                   text,
+//                   style: TextStyle(
+//                     fontSize: 40,
+//                     fontWeight: FontWeight.bold,
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//         Divider(thickness: 3, color: Colors.black),
+//       ],
+//     );
+//   }
+// }
+//
+//
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:project/Calender/calender.dart';
-import 'package:project/Main/Second.dart';
+import 'package:project/Missing/Missing.dart';
 import 'package:project/Quiz/QuizSelect.dart';
-import 'package:project/aasd/teste.dart';
 import 'package:project/components/custom_banner.dart';
 import 'package:project/components/custom_appbar.dart';
 import 'package:project/components/custom_bottomNavigation.dart';
@@ -23,133 +124,44 @@ class MH extends StatelessWidget {
 class MainHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Color appBarColor = Color.fromRGBO(121, 159, 165, 1.0);
     return Scaffold(
       appBar: CustomAppBar(),
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            InkWell(
-              onTap: () {
-                Get.to(() => CalenderApp());
-              },
-              child: Container(
-                width: 400,
-                height: 100,
-                color: Colors.white10,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Icon(
-                      Icons.calendar_month,
-                      size: 45,
-                      color: Colors.black,
-                    ),
-                    SizedBox(width: 20),
-                    Text(
-                      "돌봄 다이어리",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+            _buildMenuItem(
+              icon: Icons.calendar_month,
+              text: "돌봄 다이어리",
+              onTap: () => Get.to(() => CalenderApp()),
             ),
-            Divider(thickness: 3, color: Colors.black),
-            InkWell(
-              onTap: () {
-                Get.to(() => CenterSearch());
+            _buildMenuItem(
+              icon: Icons.grain,
+              text: "데이케어 센터 검색",
+              onTap: () async {
+                try {
+                  await NaverMapSdk.instance.initialize(
+                    clientId: "ckwq0q1fl6",  // 자신의 네이버 API 키로 교체
+                    onAuthFailed: (ex) {
+                      debugPrint("********* 네이버맵 인증오류 : $ex *********");
+                    },
+                  );
+                  Get.to(() => NMAP());
+                } catch (e) {
+                  debugPrint("NaverMap initialization error: $e");
+                }
               },
-              child: Container(
-                width: 400,
-                height: 100,
-                color: Colors.white10,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Icon(
-                      Icons.grain,
-                      size: 40,
-                      color: Colors.black,
-                    ),
-                    SizedBox(width: 20),
-                    Text(
-                      "데이케어 센터 검색",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 40,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
             ),
-            Divider(thickness: 3, color: Colors.black),
-            InkWell(
-              onTap: () {
-                Get.to(() => QuizSelect());
-              },
-              child: Container(
-                width: 400,
-                height: 100,
-                color: Colors.white10,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Icon(
-                      Icons.family_restroom,
-                      size: 40,
-                      color: Colors.black,
-                    ),
-                    SizedBox(width: 20),
-                    Text(
-                      "우리 가족 퀴즈",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+            _buildMenuItem(
+              icon: Icons.family_restroom,
+              text: "우리 가족 퀴즈",
+              onTap: () => Get.to(() => QuizSelect()),
             ),
-            Divider(thickness: 3, color: Colors.black),
-            InkWell(
-              onTap: () {
-                Get.to(() => Second());
-              },
-              child: Container(
-                width: 400,
-                height: 100,
-                color: Colors.white10,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Icon(
-                      Icons.emoji_people,
-                      size: 40,
-                      color: Colors.black,
-                    ),
-                    SizedBox(width: 20),
-                    Text(
-                      "어르신 길 잃음 방지",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+            _buildMenuItem(
+              icon: Icons.emoji_people,
+              text: "어르신 길 잃음 방지",
+              onTap: () => Get.to(() => Missing()),
             ),
-            Divider(thickness: 3, color: Colors.black),
             SizedBox(height: 23),
             Container(
               height: 140,
@@ -159,6 +171,94 @@ class MainHome extends StatelessWidget {
         ),
       ),
       bottomNavigationBar: CustomBottom(),
+    );
+  }
+
+  Widget _buildMenuItem({
+    required IconData icon,
+    required String text,
+    required VoidCallback onTap,
+  }) {
+    return Column(
+      children: [
+        InkWell(
+          onTap: onTap,
+          child: Container(
+            width: 400,
+            height: 100,
+            color: Colors.white10,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Icon(icon, size: 45, color: Colors.black),
+                SizedBox(width: 20),
+                Text(
+                  text,
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Divider(thickness: 3, color: Colors.black),
+      ],
+    );
+  }
+}
+
+class NMAP extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return NaverMap(
+      options: NaverMapViewOptions(
+        indoorEnable: false,
+        locationButtonEnable: true,
+        scrollGesturesEnable: true,
+        consumeSymbolTapEvents: true,
+        initialCameraPosition: NCameraPosition(
+          target: NLatLng(37.5666, 126.979),
+          zoom: 10,
+          bearing: 0,
+          tilt: 0,
+        ),
+        mapType: NMapType.basic,
+        activeLayerGroups: [NLayerGroup.building, NLayerGroup.transit],
+      ),
+      onMapReady: (Controller) {
+        final marker = NMarker(
+            id: 'test',
+            position: NLatLng(37.506932467450326, 127.05578661133796));
+        final marker1 = NMarker(
+            id: 'test1',
+            position: NLatLng(37.606932467450326, 127.05578661133796));
+        final marker2 = NMarker(
+            id: "test2",
+            position: NLatLng(37.654580, 127.060594));
+        final marker3 = NMarker(id: "test4", position: NLatLng(
+            37.568337, 126.897679));
+        Controller.addOverlayAll({marker, marker1,marker2,marker3});
+
+        final OnMarkerInfoMap1 =
+        NInfoWindow.onMarker(id: marker1.info.id, text: "test1");
+        marker1.openInfoWindow(OnMarkerInfoMap1);
+        final OnMarkerInfoMap12 =
+        NInfoWindow.onMarker(id: marker2.info.id, text: "test3");
+        marker2.openInfoWindow(OnMarkerInfoMap12);
+        final OnMarkerInfoMap =
+        NInfoWindow.onMarker(id: marker.info.id, text: "test2");
+        marker.openInfoWindow(OnMarkerInfoMap);
+
+        final OnMarkerInfoMap2 =
+        NInfoWindow.onMarker(id: marker3.info.id, text: "test4");
+        marker3.openInfoWindow(OnMarkerInfoMap2);
+
+      },
+      onMapTapped: (point, latLng) {
+        debugPrint("${latLng.latitude}、${latLng.longitude}");
+      },
     );
   }
 }
